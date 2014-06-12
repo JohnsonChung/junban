323f119dba8bf9b50fe0453a0a764f617c7a628f
touhonoob <touhonoob@gmail.com>
2014-06-12 19:05:57 +0800

    completed sign step

--- kiosk-demo/kiosk-demo.js
+++ kiosk-demo/kiosk-demo.js
@@ -19,7 +19,80 @@ app.controller("DemoCtrl", function($scope) {
 
     $scope.parties = 0;
 
+    $scope.init = function() {
+        $scope.reset();
+    }
+
+    $scope.reset = function() {
+        //remove canvas storage
+        window.localStorage.removeItem('drawing-board-sign-canvas');
+        
+        //reset sign flags
+        $scope.signAnimationPlayed = false;
+        $scope.signed = false;
+    }
+
+    $scope.step = 0;
+    $scope.goto = function(index) {
+        $scope.step = index;
+        $scope.template = $scope.templates[index];
+    }
+    $scope.nextStep = function() {
+        $scope.goto($scope.step + 1);
+    }
+    $scope.prevStep = function() {
+        $scope.goto($scope.step - 1);
+    }
+    $scope.goHome = function() {
+        $scope.reset();
+        $scope.goto(0);
+    }
+
+    $scope.setSignAnimationPlayed = function() {
+        $scope.signAnimationPlayed = true;
+    };
+    
+    $scope.setSigned = function(signed) {
+        $scope.signed = signed;
+    }
+});
+
+app.controller("SignCtrl", function($scope) {
+    $scope.init = function() {
+        //check instruction animation played
+        if ($scope.$parent.signAnimationPlayed) {
+            $scope.removeAnimation();
+        } else {
+            $scope.$parent.setSignAnimationPlayed();
+        }
+    };
+
+    $scope.removeAnimation = function() {
+        $(".stylie,#svg-signature").remove();
+    };
+
+    $scope.resetSignCanvas = function() {
+        $scope.signCanvas.reset({background: true});
+        $scope.$parent.setSigned(false);
+    };
+    
+    $scope.startDrawing = function() {
+        $scope.removeAnimation();
+        $scope.$parent.setSigned(true);
+    };
+    
+    $scope.nextStep = function() {
+        if(!$scope.$parent.signed) {
+            alert("Please sign first!");
+        } else {
+            $scope.$parent.nextStep();
+        }
+    }
+});
+
+app.controller("QuestionCtrl", function($scope) {
     $scope.question_index = 0;
+
     $scope.questions = [
         {
             question: "Question A",
@@ -34,6 +107,7 @@ app.controller("DemoCtrl", function($scope) {
             ]
         }
     ];
+    
     $scope.nextQuestion = function() {
         if ($scope.question_index == $scope.questions.length - 1) {
             $scope.nextStep();
@@ -41,43 +115,20 @@ app.controller("DemoCtrl", function($scope) {
             $scope.question_index++;
         }
     }
-
-    $scope.init = function() {
-    }
-
-    $scope.step = 0;
-    $scope.goto = function(index) {
-        $scope.step = index;
-        $scope.template = $scope.templates[index];
-    }
-    $scope.nextStep = function() {
-        $scope.goto($scope.step + 1);
-    }
-    $scope.prevStep = function() {
-        $scope.goto($scope.step - 1);
-    }
-});
-
-app.controller("signCtrl", function($scope) {
-    $scope.init = function() {
-
-    };
-    
-    $scope.removeAnimation = function() {
-        $(".stylie,#svg-signature").remove();
-    };
 });
 
 app.directive("signCanvas", function() {
     return {
         restrict: "A",
         link: function(scope, elem, attrs) {
-            var simpleBoard = new DrawingBoard.Board($(elem).attr('id'), {
+            scope.signCanvas = new DrawingBoard.Board($(elem).attr('id'), {
                 controls: false,
-                webStorage: false,
-                size: 15
+                webStorage: 'local',
+                size: 15,
+                background: false
             });
-            console.log(simpleBoard);
+
+            scope.signCanvas.ev.bind("board:startDrawing", scope.startDrawing);
         }
     }
 });
